c50924bf99029842e7724eb97343f3e285ab446f
Brian Travis <bztravis88@gmail.com>
2022-06-26 20:37:55 -0400

    Add 1 to cardWidth var?

--- scripts/format.js
+++ scripts/format.js
@@ -11,13 +11,15 @@ function format() {
     let contentContainerWidth = window.innerWidth - (3 * 56) - 260
     let contentContainerHeight = window.innerHeight - (2 * 56)
     let cardHeight = (contentContainerHeight / 2) - 4
-    let cardWidth = cardHeight / 3 * 2
+    console.log('cardHeight' + cardHeight)
+    let cardWidth = (cardHeight / 3 * 2) + 1
+    console.log('cardWidth' + cardWidth)
     let cardCount = Math.floor(contentContainerWidth / (cardWidth + 8)) 
     // console.log(cardCount);
     // alert(cardCount)
 
     // let flexContainerWidth = contentContainerWidth - (contentContainerWidth % (cardWidth + 8)) + 4  // 4 px error tolerance
-    let flexContainerWidth = cardCount * (cardWidth + 8) + 8// 4 px error tolerance
+    let flexContainerWidth = cardCount * (cardWidth + 8)// 4 px error tolerance
 
     // document.getElementsByClassName('card')[0].style.width = flexContainerWidth + 'px'
     // alert(cardHeight)
