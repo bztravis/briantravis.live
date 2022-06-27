642f35d28087463bde8825f367bb6c78a77dda15
Brian Travis <bztravis88@gmail.com>
2022-06-26 21:46:15 -0400

    Fix log error

--- scripts/format.js
+++ scripts/format.js
@@ -37,6 +37,30 @@ format()
 document.body.onresize = () => { format() }
 
 
+if (window.innerWidth < 600) {
+    alert('format() error')
+    return
+}
+
+// alert("resizing")
+
+let flexContainer = document.getElementsByClassName('flexContainerFormat')[0]
+// flexContainer.style.position = 'absolute'
+// flexContainer.style.background = '#444'
+
+let contentContainerWidth = window.innerWidth - (3 * 56) - 260
+let contentContainerHeight = window.innerHeight - (2 * 56)
+let cardHeight = (contentContainerHeight / 2) - 4
+console.log('cardHeight' + cardHeight)
+let cardWidth = (cardHeight / 3 * 2) + 1
+console.log('cardWidth' + cardWidth)
+let cardCount = Math.floor(contentContainerWidth / (cardWidth + 8)) 
+// console.log(cardCount);
+// alert(cardCount)
+
+// let flexContainerWidth = contentContainerWidth - (contentContainerWidth % (cardWidth + 8)) + 4  // 4 px error tolerance
+let flexContainerWidth = cardCount * (cardWidth + 8)// 4 px error tolerance
+
 setInterval(() => {
     document.getElementsByTagName('h1')[0].innerHTML = window.innerHeight
     document.getElementsByTagName('h3')[0].innerHTML = cardCount
