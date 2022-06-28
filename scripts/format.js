97f54ddbed6c6397c34cec04e0fab949a43ac49b
Brian Travis <bztravis88@gmail.com>
2022-06-28 08:51:42 -0400

    Clean format() comments

--- scripts/format.js
+++ scripts/format.js
@@ -8,13 +8,10 @@ function format() {
     let contentContainerWidth = window.innerWidth - (3 * 56) - 260
     let contentContainerHeight = window.innerHeight - (2 * 56)
     let cardHeight = (contentContainerHeight / 2) - 4
-    console.log('cardHeight' + cardHeight)
-    let cardWidth = (cardHeight / 3 * 2) + 1
+    let cardWidth = (cardHeight / 3 * 2) + 1    // 1px for error
     let cardWidthExact = (cardHeight / 3 * 2)
-    console.log('cardWidth' + cardWidth)
     let cardCount = Math.floor(contentContainerWidth / (cardWidth + 8)) 
-
-    let flexContainerWidth = cardCount * (cardWidth + 8) + 1// 4 px error tolerance
+    let flexContainerWidth = cardCount * (cardWidth + 8) + 1    // 1px for error
 
     document.documentElement.style.setProperty('--flexContainerWidth', flexContainerWidth + 'px')
     document.documentElement.style.setProperty('--cardCount', cardCount)
@@ -45,10 +42,6 @@ function checkToggleDownBtn() {
     let flexContainer = document.querySelector('.flexContainer')
     let contentContainer = document.querySelector('#contentContainer')
     let card = document.querySelector('.card')
-    // console.log(`contentContainer.scrollTop ${contentContainer.scrollTop}`);
-    // console.log(`flexContainer.offsetHeight - contentContainer.offsetHeight ${flexContainer.offsetHeight - contentContainer.offsetHeight}`)
-    console.log((flexContainer.offsetHeight - contentContainer.offsetHeight - 10) - contentContainer.scrollTop);
-    // console.log((flexContainer.offsetHeight - contentContainer.offsetHeight) - contentContainer.scrollTop <= document.querySelector('.card').offsetHeight);
     if ((flexContainer.offsetHeight - contentContainer.offsetHeight) - contentContainer.scrollTop <= card.offsetHeight) {
         downBtn.style.opacity = '0'
     } else {
