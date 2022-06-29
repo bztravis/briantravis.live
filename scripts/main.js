5765f7e0724f9d147944667624a2c11b3bbdb489
Brian Travis <bztravis88@gmail.com>
2022-06-29 13:14:13 -0400

    Add hover interactions for footerDetails

--- scripts/main.js
+++ scripts/main.js
@@ -19,3 +19,29 @@ function toggleHelpModal() {
         helpModal.style.pointerEvents = 'none'
     }
 }
+
+
+
+
+function copyToClipboard(text, classOrder) {
+
+    navigator.clipboard.writeText(text)
+
+    let targetElement = document.getElementsByClassName('footerDetail')[classOrder]
+    let initialText = targetElement.innerText
+
+    if (initialText != 'Copied to Clipboard!') {
+
+        targetElement.style.opacity = 0
+        setTimeout(() => {targetElement.style.opacity = 1}, 200)
+
+        setTimeout(() => {targetElement.innerText = 'Copied to Clipboard!'}, 200)
+
+        
+        setTimeout(() => {
+            targetElement.style.opacity = 0
+            setTimeout(() => {targetElement.style.opacity = 1}, 200)
+            setTimeout(() => {targetElement.innerText = initialText}, 200)
+        }, 2000)
+    }
+}
