6e4d89dffb28381a785b6c1bb9f1d54358a7e19a
Brian Travis <bztravis88@gmail.com>
2022-06-29 15:41:09 -0400

    Add intelligently randomized photo population

--- scripts/placePhotos.js
+++ scripts/placePhotos.js
@@ -1,9 +1,43 @@
+const photoConfig = [
+    [0, 7], // Summer
+    [8, 9] // Andy
+]
+
+
 function placePhotos() {
 
     let photoCards = document.getElementsByClassName('card')
-    for (let i = 0; i < photoCards.length; i++) {
-        photoCards[i].style.backgroundImage = `url('/images/portfolio/${1}.JPG')`
+    let currentCard = photoCards.length - 1
+
+    for (let subject = 0; subject < photoConfig.length; subject++) {
+
+        // console.log(`subject${subject}`);
+
+        let range = []
+        for (let i = photoConfig[subject][0]; i <= photoConfig[subject][1]; i++) {
+            range.push(i)
+        }
+        // console.log(range)
+
+
+        while (range.length > 0) {
+            console.log(range.length);
+            let randomPhotoIndex = Math.floor(Math.random() * range.length)
+            let photoName = range[randomPhotoIndex]
+
+            photoCards[currentCard].style.backgroundImage = `url('/images/portfolio/${photoName}.JPG')`
+            
+            // photoCards[currentCard].innerHTML = currentCard
+
+            currentCard--
+            range.splice(randomPhotoIndex, 1)   // remove element at index
+        }
     }
+    
+
+    // for (let i = 0; i < photoCards.length; i++) {
+        // photoCards[i].style.backgroundImage = `url('/images/portfolio/${photoCards.length - 1 - i}.JPG')`
+    // }
 
 }
 
